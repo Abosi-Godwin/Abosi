510434675fd202631145aa7cfd481ab6c672c543
Abosi-Godwin <bbnl6060@github.com>
2026-02-08 15:02:26 +0100

    did seo and the projects page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from "next";
-import { caseStudies } from "@/utils/assets";
+import { caseStudies } from "./utils/assets";
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = "https://abosi.vercel.app";
